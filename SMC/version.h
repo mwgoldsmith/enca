bdab7f8b6b2ac4d377a1ebed7d16918d32ccdeaa
Matthew Oliver <protogonoi@gmail.com>
2014-09-29 01:32:50 +1000

    Update VS projects to use a config.h header.

---
+++ SMC/version.h
@@ -0,0 +1,8 @@
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Enca 1.15"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.15"
+
+/* Version number of package */
+#define VERSION "1.15"
