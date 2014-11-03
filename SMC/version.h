e824cddf9c121d10d97531085834d1cc84728238
Matthew Oliver <protogonoi@gmail.com>
2014-11-03 23:18:42 +1100

    Update VS project files for recent merge.

--- SMC/version.h
+++ SMC/version.h
@@ -1,8 +1,8 @@
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Enca 1.15"
+#define PACKAGE_STRING "Enca 1.16"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.15"
+#define PACKAGE_VERSION "1.16"
 
 /* Version number of package */
-#define VERSION "1.15"
+#define VERSION "1.16"
