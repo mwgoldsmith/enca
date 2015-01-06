2b1ab4f2317bddaf5d60e35181b2c75c0a939cc2
Matt Oliver <protogonoi@gmail.com>
2015-01-06 17:19:12 +1100

    project: Cleanup config header.

--- SMP/config.h
+++ SMP/config.h
@@ -1,14 +1,3 @@
-/* Define to path to the cstocs recoder. */
-/* #undef CSTOCS_PROG */
-
-/* Define to default converter list. */
-#define DEFAULT_CONVERTER_LIST "built-in,iconv"
-
-/* Define to default external converter program. */
-#define DEFAULT_EXTERNAL_CONVERTER ""
-
-/* Define to enable external converter programs. */
-/* #undef ENABLE_EXTERNAL */
 
 /* Define to 1 if you have the `canonicalize_file_name' function. */
 /* #undef HAVE_CANONICALIZE_FILE_NAME */
@@ -167,31 +156,6 @@
 /* Define if iconv is transitive. */
 #define ICONV_IS_TRANSITIVE 1
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
-#define LT_OBJDIR ".libs/"
-
-/* Define to path to the map recoder. */
-/* #undef MAP_PROG */
-
-/* Name of package */
-#define PACKAGE "enca"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "https://github.com/nijel/enca/issues"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "Enca"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "enca"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to path to the recode recoder. */
-/* #undef RECODE_PROG */
-
 /* Define to 1 if the `S_IS*' macros in <sys/stat.h> do not work properly. */
 /* #undef STAT_MACROS_BROKEN */
 
@@ -201,63 +165,8 @@
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
 #define TIME_WITH_SYS_TIME 1
 
-/* Define to path to the umap recoder. */
-/* #undef UMAP_PROG */
-
-/* Enable extensions on AIX 3, Interix.  */
-#ifndef _ALL_SOURCE
-# define _ALL_SOURCE 1
-#endif
-/* Enable GNU extensions on systems that have them.  */
-#ifndef _GNU_SOURCE
-# define _GNU_SOURCE 1
-#endif
-/* Enable threading extensions on Solaris.  */
-#ifndef _POSIX_PTHREAD_SEMANTICS
-# define _POSIX_PTHREAD_SEMANTICS 1
-#endif
-/* Enable extensions on HP NonStop.  */
-#ifndef _TANDEM_SOURCE
-# define _TANDEM_SOURCE 1
-#endif
-/* Enable general extensions on Solaris.  */
-#ifndef __EXTENSIONS__
-# define __EXTENSIONS__ 1
-#endif
-
-
-#include "version.h"
-
-/* Define to 1 if on MINIX. */
-/* #undef _MINIX */
-
-/* Define to 2 if the system does not provide POSIX.1 features except with
-   this defined. */
-/* #undef _POSIX_1_SOURCE */
-
-/* Define to 1 if you need to in order for `stat' and other things to work. */
-/* #undef _POSIX_SOURCE */
-
-/* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
-
 /* Define to `int' if <sys/types.h> doesn't define. */
 #define gid_t int
-
-/* Define to `int' if <sys/types.h> does not define. */
-/* #undef mode_t */
-
-/* Define to `long int' if <sys/types.h> does not define. */
-/* #undef off_t */
-
-/* Define to `int' if <sys/types.h> does not define. */
-/* #undef pid_t */
-
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
-
-/* Define to `int' if <sys/types.h> does not define. */
-/* #undef ssize_t */
 
 /* Define to `int' if <sys/types.h> doesn't define. */
 #define uid_t int
--- SMP/version.h
+++
@@ -1,8 +0,0 @@
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Enca 1.16"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "1.16"
-
-/* Version number of package */
-#define VERSION "1.16"
